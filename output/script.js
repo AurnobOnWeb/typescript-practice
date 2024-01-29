3d4378d00c2b56c772abc6dfe763bee33defaefb
Abir Hossen Ornob <abirhosain090@gmail.com>
2024-01-29 21:52:45 +0600

    function return type

--- output/script.js
+++ output/script.js
@@ -1,1 +1,10 @@
 "use strict";
+// const myfunction : Function  =() =>{
+//   console.log("hellow");
+// }
+// // OPtional string >?
+// // default value 
+// const myfunction = (a:string , b?:String ,c:String ='10') =>{
+//   console.log('Hellow', a+b);
+// }
+// myfunction('a','b');
---
+++ src/script.ts
@@ -0,0 +1,14 @@
+// const myfunction : Function  =() =>{
+//   console.log("hellow");
+// }
+
+
+
+// // OPtional string >?
+// // default value 
+// const myfunction = (a:string , b?:String ,c:String ='10') =>{
+//   console.log('Hellow', a+b);
+  
+// }
+
+// myfunction('a','b');
